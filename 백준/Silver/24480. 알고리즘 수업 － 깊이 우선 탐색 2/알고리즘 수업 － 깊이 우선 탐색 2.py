0219740070562c0d954698823e0c63337ab5b566
Suhyun Rim <ish000118@gmail.com>
2024-03-20 15:51:32 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 572 ms, Memory: 67996 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,31 @@
+# silver 2
+# 알고리즘 수업 - 깊이 우선 탐색 2
+# DFS
+
+import sys
+sys.setrecursionlimit(150000)
+
+def dfs(R):
+    global order
+    visited[R] = order
+    
+    line[R].sort(reverse=True)
+    for i in line[R]:
+        if visited[i]==0:
+            order+=1
+            dfs(i)
+
+
+N, M, R = map(int, sys.stdin.readline().split())
+line = [[] for _ in range(N+1)]
+visited = [0]*(N+1)
+order = 1
+
+for i in range(M):
+    u, v = map(int, sys.stdin.readline().split())
+    line[u].append(v)
+    line[v].append(u)
+
+dfs(R)
+for i in range(1, N+1):
+    print(visited[i])
