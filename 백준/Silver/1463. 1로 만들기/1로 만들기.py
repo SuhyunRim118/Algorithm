fd44263cdc612f9cda90e297a1ff3826a73a5fd7
Suhyun Rim <ish000118@gmail.com>
2024-03-22 14:26:11 +0900

    [Silver III] Title: 1로 만들기, Time: 56 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -2,16 +2,35 @@
 # 1로 만들기
 
 import sys
+sys.setrecursionlimit(15000)
 n = int(sys.stdin.readline())
 dp = [0]*(n+1)
 
-for i in range(2, n+1):
-    dp[i] = dp[i-1]+1
+# Bottom to Top
+# for i in range(2, n+1):
+#     dp[i] = dp[i-1]+1
 
-    if i%2==0:
-        dp[i] = min(dp[i], dp[i//2]+1)
-    if i%3==0:
-        dp[i] = min(dp[i], dp[i//3]+1)
+#     if i%2==0:
+#         dp[i] = min(dp[i], dp[i//2]+1)
+#     if i%3==0:
+#         dp[i] = min(dp[i], dp[i//3]+1)
 
+# Top to Bottom
+def rec(n):
+    if n==1 or dp[n]!=0:
+        return dp[n]
+
+    if (n%3==0) and (n%2==0):
+        dp[n] = min(rec(n//3)+1, rec(n//2)+1)
+    elif n%3==0:
+        dp[n] = min(rec(n//3)+1, rec(n-1)+1)
+    elif n%2==0:
+        dp[n] = min(rec(n//2)+1, rec(n-1)+1)
+    else:
+        dp[n] = rec(n-1)+1
+    
+    return dp[n]
+
+rec(n)
 
 print(dp[n])
