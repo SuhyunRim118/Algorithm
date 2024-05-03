2f45bb6ffc399e21736adc688f2fc816430688c6
Suhyun Rim <ish000118@gmail.com>
2024-05-03 17:06:51 +0900

    [Gold IV] Title: 빙산, Time: 672 ms, Memory: 191684 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -9,6 +9,7 @@ import copy
 dr = [0, 0, -1, 1]
 dc = [-1, 1, 0, 0]
 
+# 얼음 녹이기
 def melt():
     ice_melt = [[0]*M for _ in range(N)]
 
@@ -30,6 +31,7 @@ def melt():
                 else:
                     ice[r][c] -= ice_melt[r][c]
 
+# 빙산 덩어리 개수 확인
 def bfs(r, c):
     q = deque()
     q.append((r, c))
@@ -37,25 +39,18 @@ def bfs(r, c):
 
     while q:
         r, c = q.popleft()
-        # water_cnt = 0
 
         for i in range(4):
             nr = r+dr[i]
             nc = c+dc[i]
 
             if 0<=nr<N and 0<=nc<M:
-                # if ice[nr][nc]==0:
-                #     water_cnt+=1
-                # elif 
+
                 if ice[nr][nc]!=0 and visit[nr][nc]==0:
                     q.append((nr, nc))
                     visit[nr][nc] = 1
 
-            # if ice[r][c] <= water_cnt:
-            #     next_ice[r][c] = 0
-            # else:
-            #     next_ice[r][c] = ice[r][c] - water_cnt
-
+# input
 N, M = map(int, input().split())
 ice = []
 for n in range(N):
@@ -70,19 +65,16 @@ while True:
     year+=1
 
     melt()
-    # print(ice)
 
     for n in range(1, N-1):
         for m in range(1, M-1):
             if ice[n][m]>0 and visit[n][m]==0:
                 bfs(n, m)
                 ice_cnt+=1
-                # print(ice_cnt)
 
-
-    if ice_cnt > 1:
+    if ice_cnt > 1: # 빙산 덩어리가 2개 이상일 때
         break
-    elif ice_cnt==0:
+    elif ice_cnt==0: # 빙산이 다 녹았을 때
         year = 0
         break
 
