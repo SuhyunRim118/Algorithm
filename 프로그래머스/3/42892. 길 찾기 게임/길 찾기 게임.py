d367aa389673b8599125e248703d00c9a1927943
Suhyun Rim <ish000118@gmail.com>
2024-06-29 15:58:53 +0900

    [level 3] Title: 길 찾기 게임, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
@@ -1,17 +1,18 @@
-class Node: # Node class
-    def __init__(self, xy, n, left=None, right=None):
-        self.xy = xy
+class Node:
+    def __init__(self, n, info, left=None, right=None):
         self.n = n
+        self.info = info
         self.left = left
         self.right = right
-
+        
     def has_left(self):
         return self.left
-
+    
     def has_right(self):
         return self.right
-
-def make_BT(nodeinfo): # 이진 트리 만들기
+    
+def makeBT(nodeinfo):
+    
     nodes = [i for i in range(1, len(nodeinfo)+1)]
     nodes.sort(key=lambda x: (-nodeinfo[x-1][1], nodeinfo[x-1][0]))
     
@@ -19,62 +20,61 @@ def make_BT(nodeinfo): # 이진 트리 만들기
     
     for i in range(len(nodes)):
         if root is None:
-            root = Node(nodeinfo[nodes[0]-1], nodes[0]) # 초기 root 설정
+            root = Node(nodes[0], nodeinfo[nodes[0]-1])
         else:
             parent = root
-            node = Node(nodeinfo[nodes[i]-1], nodes[i])
-            
-            while True: # node가 자리를 찾을 때까지
-                # parent보다 작은 x 값 -> 왼쪽
-                if node.xy[0]<parent.xy[0]:
+            node = Node(nodes[i], nodeinfo[nodes[i]-1])
+        
+            while True:
+                if node.info[0] < parent.info[0]:
                     if parent.has_left() is not None:
                         parent = parent.left
                         continue
                     parent.left = node
                     break
-                # parent보다 큰 x 값 -> 오른쪽
                 else:
                     if parent.has_right() is not None:
                         parent = parent.right
                         continue
                     parent.right = node
                     break
-            
-    return root       
     
-def pre_order(root, answer): # 전위 순회
-    stack = [root]
+    return root
+
+def preorder(root, answer):
+    stk = [root]
     
-    while stack:
-        node = stack.pop()
+    while stk:
+        node = stk.pop()
         
         if node is None:
             continue
-            
+        
         answer[0].append(node.n)
-        stack.append(node.right)
-        stack.append(node.left)
+        stk.append(node.right)
+        stk.append(node.left)
     
-def post_order(root, answer): # 후위 순회
-    stack = [(root, False)]
+def postorder(root, answer):
+    stk = [(root, False)]
     
-    while stack:
-        node, visit = stack.pop()
+    while stk:
+        node, visit = stk.pop()
         
         if node is None:
             continue
+            
         if visit:
             answer[1].append(node.n)
         else:
-            stack.append((node, True))
-            stack.append((node.right, False))
-            stack.append((node.left, False))
+            stk.append((node, True))
+            stk.append((node.right, False))
+            stk.append((node.left, False))
         
 def solution(nodeinfo):
     answer = [[], []]
+    
+    root = makeBT(nodeinfo)
+    preorder(root, answer)
+    postorder(root, answer)
 
-    root = make_BT(nodeinfo)
-    pre_order(root, answer)
-    post_order(root, answer)
-        
     return answer
