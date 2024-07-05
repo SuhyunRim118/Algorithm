0d66ca03c7319a78a76e9a9b825aadecdb6c078c
Suhyun Rim <ish000118@gmail.com>
2024-07-05 20:53:31 +0900

    [level 3] Title: 네트워크, Time: 0.12 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,31 +1,20 @@
 from collections import deque
 
-def bfs(start, computers, visit, n):
-    q = deque([start])
+def dfs(node, visit, computers):
+    visit[node] = True
     
-    while q:
-        x, y = q.popleft()
-        
-        for i in range(n): # 연결되어 있는 컴퓨터
-            if computers[x][i]==1 and i not in visit:
-                visit.add(i)
-                q.append([i, x])
+    for i, connect in enumerate(computers[node]):
+        if connect and not visit[i]:
+            dfs(i, visit, computers)
     
 def solution(n, computers):
     answer = 0
-    visit = set()
-    cnt = 0
+    visit = [False]*n
     
-    for y in range(n):
-        for x in range(n):
-            if x==y:
-                continue
-            if computers[y][x]==1 and x not in visit: # 다른 컴퓨터와 연결되어 있는
-                visit.add(x)
-                cnt+=1
-                bfs([x, y], computers, visit, n)
-    
-    # # 연결되어 있지 않은 컴퓨터
-    left = n-len(visit)
+    for i in range(n):
+        
+        if not visit[i]:
+            dfs(i, visit, computers)
+            answer+=1
 
-    return cnt+left
+    return answer
